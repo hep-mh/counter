3b93f6f74d00cf0d8c6d54ed0c6f234dbc614b74
hep-mh <mail@hep-mh.com>
2022-09-05 22:28:21 +0200

    better build process

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+cp -r assets build/assets
 javac main.java
-jar cfm counter.jar MANIFEST.MF *.class lib/* assets/*
-chmod 755 counter.jar
+jar cfm build/counter.jar MANIFEST.MF *.class assets/*
+chmod 755 build/counter.jar
